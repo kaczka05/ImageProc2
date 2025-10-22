00f18867c44ed3f80c9ab9df1ace6d775bcb3a6a
Jakub Babaniuk <jbabaniuk@gmail.com>
2025-10-22 12:40:58 +0200

    noise processed

--- similarity.h
+++ similarity.h
@@ -5,4 +5,20 @@
 #ifndef SIMILARITY_H
 #define SIMILARITY_H
 
+#include "CImg.h"
+#include <limits>
+
+struct SimilarityMetrics {
+    double MSE;
+    double PMSE;
+    double SNR;   // in dB
+    double PSNR;  // in dB
+    double MD;    // maximum absolute difference
+};
+
+// Compute all metrics. Images must have same size & channels.
+SimilarityMetrics compute_similarity(const cimg_library::CImg<unsigned char>& original,
+                                     const cimg_library::CImg<unsigned char>& processed);
+
+
 #endif //SIMILARITY_H
