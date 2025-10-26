171cfc1dcc8b6ce2eb347fdb1903f1d5f2e1ef6c
DragoDam <widstarasd1@gmail.com>
2025-10-26 21:37:09 +0100

    final version

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include  "funtions.h"
 #include "similarity.h"
+#include <cstdlib>
 
 
 using namespace std;
@@ -11,10 +12,9 @@ using namespace cimg_library;
 int main() {
 
     string file_name, command_name;
-    int Iargument;
-    float Fargument;
+    string input, input2;
     //cin >> file_name; do odkomentowania jak bedzie trzeba
-    //cin.ignore(3)
+    //cin.ignore(2);
     do {
         cout << "Write your command in fromat '[file name] [--command] [--argument=value] ... ' use --help to learn the commands " << '\n';
 
@@ -25,22 +25,22 @@ int main() {
         CImg<unsigned char> image(file_name.c_str());
         CImg<unsigned char> orig = image;
 
-
+        cin.ignore(3);
         cin >> command_name;
 
 
 
         if (command_name == "brightness" || command_name == "b") {
-            //cin.ignore(3);
-
-            cin >> Iargument;
-            brightness(image, Iargument);
+            cin.ignore(8);
+            cin >> input;
+            cout << input << endl;
+            brightness(image, stoi(input));
         }
 
         else if (command_name == "contrast" || command_name == "c") {
-            //cin.ignore(3);
-            cin >> Fargument;
-            contrast(image, Fargument);
+            cin.ignore(9);
+            cin >> input;
+            contrast(image, stoi(input));
         }
 
         else if (command_name == "negative" || command_name == "n") {
@@ -66,16 +66,20 @@ int main() {
         }
 
         else if (command_name == "change_size" || command_name == "chs") {
-            int newW, newH;
-            cin >> newW >> newH;
-            change_size(image, newW, newH);
+            cin.ignore(8);
+            cin >> input;
+            cin.ignore(9);
+            cin >> input2;
+            change_size(image, stoi(input), stoi(input2));
         }
 
         else if (command_name == "adaptive" || command_name == "a") {
-            cin >> Iargument;
-            int minSize = Iargument;
-            cin >> Iargument;
-            int maxSize = Iargument;
+            cin.ignore(6);
+            cin >> input;
+            int minSize = stoi(input);
+            cin.ignore(6);
+            cin >> input;
+            int maxSize = stoi(input);
             adaptive_filter(image, minSize, maxSize);
 
             CImg<unsigned char> image_out("out.bmp");
@@ -83,40 +87,38 @@ int main() {
         }
 
         else if (command_name == "min" ) {
-            cin >> Iargument;
-            min_filter(image, Iargument);
-
-
+            cin.ignore(7);
+            cin >> input;
+            min_filter(image, stoi(input));
         }
 
         else if (command_name == "max" ) {
-            cin >> Iargument;
-            max_filter(image, Iargument);
-
-
+            cin.ignore(7);
+            cin >> input;
+            max_filter(image, stoi(input));
         }
 
         else if ( command_name == "test") {
             string file_name1, file_name2;
-            cin>>file_name1>>file_name2;
+            cin>>file_name2;
 
-            CImg<unsigned char> test1(file_name1.c_str());
+            CImg<unsigned char> test1(file_name.c_str());
             CImg<unsigned char> test2(file_name2.c_str());
             compare_similarity(test1, test2);
         }
 
         else if (file_name == "help" || file_name == "h" || file_name == "--help") {
             cout << "\n=== AVAILABLE COMMANDS ===\n";
-            cout << "File format: [filename] [--command] [--argument=value] ...\n\n";
+            cout << "Input format: [filename] [--command] [--argument=value] ...\n\n";
 
             cout << "General:\n";
             cout << "  help | h                     : Show this help message.\n";
             cout << "  quit | q                     : Exit the program.\n\n";
 
             cout << "Image operations:\n";
-            cout << "  brightness | b <int>         : Adjust brightness by given value.\n";
+            cout << "  brightness | b value<int>    : Adjust brightness by given value.\n";
             cout << "                                 Positive = brighter, Negative = darker.\n";
-            cout << "  contrast | c <float>         : Change contrast by a factor.\n";
+            cout << "  contrast | c factor<float>         : Change contrast by a factor.\n";
             cout << "                                 Example: 1.2 = increase, 0.8 = decrease.\n";
             cout << "  negative | n                 : Invert image colors.\n";
             cout << "  horizontal_flip | hf         : Flip the image horizontally.\n";
@@ -124,6 +126,8 @@ int main() {
             cout << "  diagonal_flip | df           : Flip along the main diagonal.\n";
             cout << "  enlarge | en <width> <height>: Resize image to new dimensions.\n";
             cout << "  adaptive | a <min> <max>     : Apply adaptive filter with min/max window size.\n";
+            cout << "  min <size>                   : Apply minimum filter with given window size. \n";
+            cout << "  max <size>                   : Apply maximum filter with given window size. \n";
         }
 
         //image = outputImage;
