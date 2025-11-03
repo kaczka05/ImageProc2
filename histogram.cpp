2762ed446cfb0256d2ac2d4f41aa3fdf87989d89
DragoDam <widstarasd1@gmail.com>
2025-11-03 11:42:15 +0100

    histogram ready

--- histogram.cpp
+++ histogram.cpp
@@ -8,23 +8,31 @@ using namespace std;
 using namespace cimg_library;
 
 void create_histogram(CImg<unsigned char>& image) {
+    int max = 0;
     float histogram [256]  = {0} ;
     for (int x = 0 ; x < image.width() ; x++) {
         for (int y = 0 ; y < image.height() ; y++ ) {
             histogram[image(x,y,0)] ++;
         }
     }
-    int histHeight = 100;
-    CImg<unsigned char> histImg(256, 100, 1, 3, 255); // white background
+    for (int x = 0 ; x < 256 ; x++) {
+        if ( histogram[x] > max ) {
+            max = histogram[x];
+        }
+    }
+    int histHeight = 300;
+    CImg<unsigned char> histImg(256, 300, 1, 3, 255); // white background
 
     // 3️⃣ Draw vertical bars
     for (int i = 0; i < 256; i++) {
-        int barHeight = (int)(histogram[i] * histHeight);
-        for (int y = histHeight - 1; y >= histHeight - barHeight; y--) {
-            histImg(i, y, 0) = 0;   // Red channel
-            histImg(i, y, 1) = 0;   // Green
-            histImg(i, y, 2) = 0;   // Blue (→ black bar)
+        int barHeight = (histogram[i]*histHeight)/(max+1);
+        for (int j=0; j<=barHeight; j++) {
+
+            histImg(i, histHeight-barHeight-1+j, 0) = 0;   // Red channel
+            histImg(i, histHeight-barHeight-1+j, 1) = 0;   // Green
+            histImg(i, histHeight-barHeight-1+j, 2) = 0;   // Blue (→ black bar)
         }
+
     }
-    image.save_bmp("histogram.bmp");
+    histImg.save_bmp("histogram.bmp");
 }
